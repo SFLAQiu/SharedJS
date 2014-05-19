6dc61ce95dfa7e884599f219b848a52c200b10f8
SFLAQiu <563644741@qq.com>
2014-05-19 23:10:02 +0800

    第一版本
    历史性的一刻

---
+++ SFLYQ.Helper.js
@@ -0,0 +1,99 @@
+﻿/*
+**** JavaScript Document
+**** SFLYQ
+**** 2014-3-10
+*/
+/// <reference path="/JS/jquery.1.9.1.min.js" />
+if (typeof SFLYQ == 'undefined') {
+    var SFLYQ = {};
+}
+(function () {
+	/// <summary>所有帮助操作</summary>
+    SFLYQ.Helper = {};
+    //----文件帮助
+    SFLYQ.Helper.FileHander = {};
+    var fh = SFLYQ.Helper.FileHander;
+    fh.checkFileExtension = function (allowExtensionArr, filePath) {
+    	/// <summary>判断文件格式</summary>
+		/// <param name="allowExtensionArr" type="array">允许图片格式字符串数组,如：['jpg','png']</param>
+		/// <param name="filePath" type="string">文件路径名，包括文件名</param>
+    	if (!filePath) return false;
+        for (var i = 0; i < allowExtensionArr.length; i++) {
+            var reStr = "^.*\." + allowExtensionArr[i] + "$";
+            var re = RegExp(reStr);
+            if (filePath.toLowerCase().match(re) != null) return true;
+        }
+        return false;
+    };
+    //----字符串帮助
+    SFLYQ.Helper.StringHander = {};
+    var sh = SFLYQ.Helper.StringHander;
+    sh.clearSpace = function (str) {
+        /// <summary>清理字符串空格</summary>
+        /// <param name="str" type="string">字符串</param>
+        return str.replace(/\s/g, "");
+    };
+    sh.numStrToTallyDif = function (num) {
+        /// <summary>数字字符串，数位用','隔开。如得到字符串"3,444,567,123"</summary>
+        /// <param name="num" type="int"></param>
+        var numStr = String(num);
+        if (!numStr) return numStr;
+        var re = /(\d{1,3})(?=(\d{3})+(?:$|\.))/g;
+        return numStr.replace(re, "$1,");
+    };
+    sh.getUrlObj = function (urlStr) {
+        /// <summary>通过url解析获取url对象</summary>
+        /// <param name="url" type="string"></param>
+        var _fields = {
+            'Username': 4,
+            'Password': 5,
+            'Port': 7,
+            'Protocol': 2,
+            'Host': 6,
+            'Pathname': 8,
+            'URL': 0,
+            'Querystring': 9,
+            'Fragment': 10
+        };
+        var values = {};
+        var regex = /^((\w+):\/\/)?((\w+):?(\w+)?@)?([^\/\?:]+):?(\d+)?(\/?[^\?#]+)?\??([^#]+)?#?(\w*)/;
+        if (typeof urlStr != 'undefined') {
+            for (var f in _fields) {
+                values[f] = '';
+            }
+            var r = regex.exec(urlStr);
+            if (!r) return; // throw "DPURLParser::parse -> Invalid URL";
+            for (var f in _fields) if (typeof r[_fields[f]] != 'undefined') {
+                values[f] = r[_fields[f]];
+            }
+        }
+        return values;
+    }
+    sh.getParamesObjByUrl = function (url) {
+        /// <summary>通过url获取参数对象</summary>
+        /// <param name="url" type="string"></param>
+        if (!url) return null;
+        var indexNum = url.indexOf("?");
+        if (indexNum <= 0) return null;
+        var paramesStr = url.substring(indexNum + 1, url.length)
+        var pattern = /([^&]+)=([^&]+)/g;//定义正则表达式
+        var parames = {};//定义数组
+        if (paramesStr.match(pattern) == "") return null;
+        paramesStr.replace(pattern, function (a, b, c) {
+            parames[b] = c;
+        });
+        return parames;//返回这个对象.
+    };
+    //----选择器帮助
+    SFLYQ.Helper.SelectorHander= {};
+    var sStor = SFLYQ.Helper.SelectorHander;
+    sStor.getSelectorStrByAttr = function (selField, selValue) {
+        /// <summary>根据属性名和查询的值，返回选模糊属性择器字符串</summary>
+        /// <param name="selField" type="string">查询字段名</param>
+        /// <param name="selValue" type="string">查询值</param>
+        if (selValue == "") return "";
+        var getValue = selValue.replace(/\s+/g, "");
+        if (getValue == "") return "";
+        return "[" + selField + "*='" + getValue + "']";
+    };
+})();
---
+++ SFLYQ.Referrer.js
@@ -0,0 +1,84 @@
+﻿/*
+**** JavaScript Document
+**** SFLYQ
+**** 2014-3-19
+**** 搜索来源关键字获取
+*/
+/// <reference path="/JS/jquery.1.9.1.min.js" />
+/// <reference path="/JS/SFLYQ.Helper.js" />
+if (typeof SFLYQ == 'undefined') {
+	var SFLYQ = {};
+}
+(function () {
+	if (!SFLYQ.Referrer) SFLYQ.Referrer = {};
+	var sr = SFLYQ.Referrer;
+	//父类
+	sr.Parent = function () { };
+	sr.Parent.prototype.GetReferrerKeyWord = function (urlParameterObj) { }; //根据url参数集合，返回来源关键字字符串
+	//工厂类
+	sr.Factory = function () {
+		this.getReferrerObj = function (domainName) {
+			switch (domainName) {
+				case baiduReferrer.domainName:
+					return baiduReferrer;
+				case googleReferrer.domainName:
+					return googleReferrer;
+				default:
+					return null;
+			}
+			return null;
+		}
+	};
+	sr.DoAllHander = function (roallBack, failureAnalysisBack) {
+        /// <summary>执行来源分析和操作</summary>
+	    /// <param name="roallBack" type="Object">解析成功执行函数</param>
+	    /// <param name="failureAnalysisBack" type="Object">解析失败执行函数(如果无来源也视为解析失败)</param>
+	    var keyword=sr.DoAnalysis();
+	    if (keyword) {
+	        if (roallBack) roallBack(keyword);
+	    } else {
+	        if (failureAnalysisBack) failureAnalysisBack()
+	    }
+	};
+	sr.DoAnalysis = function () {
+        /// <summary>解析操作</summary>
+	    var sh = SFLYQ.Helper;
+	    var keyword = "";
+	    var factoryObj = new sr.Factory();
+	    //来源URL是否存在
+	    var referrerUrl = decodeURIComponent(document.referrer);
+	    if (!referrerUrl) return keyword;
+	    //来源域名是否存在,来源URL参数对象
+	    var urlObj = sh.StringHander.getUrlObj(referrerUrl);
+	    if (!urlObj) return keyword;
+	    var domainName = urlObj.Host;
+	    if (!domainName) return keyword;
+	    var urlParamesObj = sh.StringHander.getParamesObjByUrl(referrerUrl);
+	    if (!urlParamesObj) return keyword;
+	    //来源域名是否存在
+	    var referrerObj = factoryObj.getReferrerObj(domainName);
+	    if (!referrerObj) return keyword;
+	    //获取当前来源域名对象
+	    keyword = referrerObj.GetReferrerKeyWord(urlParamesObj);
+	    if (!keyword) return keyword;
+	    return keyword;
+	};
+
+
+	//百度
+	var baiduReferrer = new sr.Parent();
+	baiduReferrer.domainName = "www.baidu.com";
+	baiduReferrer.GetReferrerKeyWord = function (urlParameterObj) {
+		if (!urlParameterObj) return "";
+		if (!urlParameterObj.wd) return "";
+		return decodeURI(urlParameterObj.wd);
+	};
+	//谷歌
+	var googleReferrer = new sr.Parent();
+	googleReferrer.domainName = "www.google.com.hk";
+	googleReferrer.GetReferrerKeyWord = function (urlParameterObj) {
+		if (!urlParameterObj) return "";
+		if (!urlParameterObj.q) return "";
+		return decodeURI(urlParameterObj.q);
+	};
+})();
